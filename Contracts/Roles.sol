5b6aaf68793e1556418bd8015a8b7a7f27627a6e
kishansudani <48625369+kishansudani@users.noreply.github.com>
2021-02-24 15:42:47 +0530

    Update Roles.sol

--- Contracts/Roles.sol
+++ Contracts/Roles.sol
@@ -9,6 +9,11 @@ library Roles {
         mapping (address => string) area;
     }
     
+    struct ForAdding {
+        mapping (address => bool) canPersonAddVoter;
+        mapping (address => bool) canPersonAddNominee;
+    }
+    
     function add(Role storage role, address _account, string memory _addharCard, string memory _area) internal {
         require(_account != address(0));
         require(!has(role, _account));
@@ -25,6 +30,30 @@ library Roles {
         role.bearer[_account] = false;
         role.addharCard[_account] = "";
         role.area[_account] = "";
+    }
+    
+    function addForVoter(ForAdding storage adding, address _account) internal {
+        require(_account != address(0));
+        require(!adding.canPersonAddVoter[_account]);
+        adding.canPersonAddVoter[_account] = true;
+    }
+    
+    function addForNominee(ForAdding storage adding, address _account) internal {
+        require(_account != address(0));
+        require(!adding.canPersonAddNominee[_account]);
+        adding.canPersonAddNominee[_account] = true;
+    }
+    
+    function removeForVoter(ForAdding storage adding, address _account) internal {
+        require(_account != address(0));
+        require(adding.canPersonAddVoter[_account]);
+        adding.canPersonAddVoter[_account] = false;
+    }
+    
+    function removeForNominee(ForAdding storage adding, address _account) internal {
+        require(_account != address(0));
+        require(adding.canPersonAddNominee[_account]);
+        adding.canPersonAddNominee[_account] = false;
     }
     
     function has(Role storage role, address account) internal view returns(bool) {
